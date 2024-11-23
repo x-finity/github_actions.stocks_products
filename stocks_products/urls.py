74734ff8e350294c0cf354a3be19902716b7d0dd
x-finity <x.finity777@gmail.com>
2024-11-24 01:41:57 +0300

    url's bugfix

--- stocks_products/urls.py
+++ stocks_products/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('logistic.urls')),
+    path('netology/api/v1/', include('logistic.urls')),
 ]
